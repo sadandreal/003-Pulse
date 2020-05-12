c20da5c4878343bf4fa60fc0b13951ae7e549523
sadandreal <mariaadesigns@gmail.com>
2020-05-12 13:37:48 +0300

    pochti ideal

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
     speed: 1200,
     // adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="dist/icons/arrow_left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="dist/icons/arrow_right.png"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>',
     responsive: [
         {
         breakpoint: 768,
